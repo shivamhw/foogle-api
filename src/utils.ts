4c145a6157f26b3b0c83180aeaa7d904ed39bbf3
Shivam <s.kmishra703@gmail.com>
2023-10-23 23:42:02 +0530

    Added foogle reponse types and npm prune

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,14 @@
 import { drive_v3 } from "googleapis";
-
-
-interface foogleResponse extends drive_v3.Schema$File{
-    cf_worker_link?: string
-    player_link?: string
-}
+import { FoogleMovieResponse } from "./types";
 
 
 
 export function processResults(result: drive_v3.Schema$File[][] ){
-    let files: foogleResponse[] = [];
+    let files: FoogleMovieResponse[] = [];
     let name_idx = new Set()
     let id_idx = new Set()
     result.map((r: drive_v3.Schema$FileList[] ) => {
-        r.map((file: foogleResponse) =>{
+        r.map((file: FoogleMovieResponse) =>{
             if( !id_idx.has(file.id)){
                 file.cf_worker_link = process.env.CF_URL === undefined? "nourl/" : process.env.CF_URL + file.id;
                 file.player_link = file.cf_worker_link + "/" + encodeURIComponent(file?.name == undefined ? "" : file.name);
